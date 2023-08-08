d7226ace3e0ad267e98524e7b663cc62cef657a6
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2023-08-08 00:31:12 -0400

    Made it look better

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,11 +1,12 @@
+import { ImCross } from "react-icons/im";
 import React from "react";
 
 const Expense = () => {
   return (
     <div className="bg-neutral-focus p-3 rounded-lg">
       <span className="flex justify-between">
-        <button className="bg-red-600 px-2 py-1 rounded-lg align-bottom">
-          X
+        <button className="bg-red-600 p-2 rounded-lg align-bottom">
+          <ImCross />
         </button>
         <p className="inline mt-1">Text</p>
         <p className="inline mt-1">$XX.XX</p>
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -3,7 +3,14 @@ import Expense from "./Expense";
 
 const ExpenseList = () => {
   return (
-    <div className=" bg-secondary-content p-4 mx-[30%]">
+    <div className="flex flex-col gap-3 max-h-72 overflow-scroll bg-secondary-content p-4 mx-[30%] rounded-b-lg">
+      <Expense />
+      <Expense />
+      <Expense />
+      <Expense />
+      <Expense />
+      <Expense />
+      <Expense />
       <Expense />
     </div>
   );
